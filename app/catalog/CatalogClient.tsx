be3cf3abab68658d0c65a6f8515a424465c6a1c3
Inna Saienko <inna.saienko.nl@gmail.com>
2026-09-10 13:01:23 +0200

    feat: add Load more button to CarList with pagination support

--- app/catalog/CatalogClient.tsx
+++ app/catalog/CatalogClient.tsx
@@ -4,25 +4,37 @@ import {keepPreviousData, useInfiniteQuery} from "@tanstack/react-query";
 import {fetchCars} from "@/lib/api";
 import CarList from "@/components/CarList/CarList";
 import {FetchCarsResponse} from "@/lib/api";
+import Button from "@/components/Button/Button";
 
 export const catalogPerPage = 8;
 
 const CatalogClient = () => {
 
-    const {data} = useInfiniteQuery<FetchCarsResponse>({
+    const {data, hasNextPage} = useInfiniteQuery<FetchCarsResponse>({
         queryKey: ["cars"],
-        queryFn: ({} ) => fetchCars({page: 1, perPage: catalogPerPage}),
+        queryFn: ({}) => fetchCars({page: 1, perPage: catalogPerPage}),
         initialPageParam: 1,
         getNextPageParam: (lastPage) => lastPage.page < lastPage.totalPages ? lastPage.page + 1 : undefined,
         placeholderData: keepPreviousData,
         refetchOnMount: false,
 
     })
-    
+
     const allCars = data?.pages.flatMap((page) => page.cars) || [];
-    
+
     return (
-        <CarList cars={allCars} />
+        <>
+            <CarList cars={allCars}/>
+            {hasNextPage && (
+                    <Button
+                        variant="loadMore"
+                        // onClick={() => fetchNextPage()}
+                        className="loadMore"
+                    >Load more
+                    </Button>
+                )
+            }
+        </>
     )
 }
 export default CatalogClient;
