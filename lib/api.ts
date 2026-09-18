a6807be7584d17012345beca71f46d6cb61506c1
Inna Saienko <inna.saienko.nl@gmail.com>
2026-09-18 10:20:32 +0200

    feat: add custom favicon and improve metadata

--- lib/api.ts
+++ lib/api.ts
@@ -65,7 +65,6 @@ export const fetchCarFilters = async(): Promise<CarFiltersResponse> => {
 
 export const fetchCarById = async (id: string): Promise<Car> => {
     const response: AxiosResponse<Car> = await carApi.get(`/cars/${id}`);
-    console.log("Response by ID: ", response);
 
     return response.data;
 };
